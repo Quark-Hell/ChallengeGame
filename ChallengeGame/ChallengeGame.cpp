49a5b687ced42b9a9615d2737a7a37300d07aeec
Dinamitic <gennadiy.kaydalov@mail.ru>
2020-09-18 19:53:25 +0300

    движение работает нормально.Стрельба не реализована

--- ChallengeGame/ChallengeGame.cpp
+++ ChallengeGame/ChallengeGame.cpp
@@ -4,11 +4,55 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <conio.h>
 #include <windows.h>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
 #pragma region Заголов.h
+class Timer
+{
+public:
+    void start()
+    {
+        m_StartTime = std::chrono::system_clock::now();
+        m_bRunning = true;
+    }
+
+    void stop()
+    {
+        m_EndTime = std::chrono::system_clock::now();
+        m_bRunning = false;
+    }
+
+    double elapsedMilliseconds()
+    {
+        std::chrono::time_point<std::chrono::system_clock> endTime;
+
+        if (m_bRunning)
+        {
+            endTime = std::chrono::system_clock::now();
+        }
+        else
+        {
+            endTime = m_EndTime;
+        }
+
+        return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
+    }
+
+    double elapsedSeconds()
+    {
+        return elapsedMilliseconds() / 1000.0;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> m_StartTime;
+    std::chrono::time_point<std::chrono::system_clock> m_EndTime;
+    bool                                               m_bRunning = false;
+};
 #pragma region EngineTyps
 class GameObject {
 public:
@@ -19,7 +63,7 @@ public:
 class Draw {
 public:
     void EreseObject(COORD coord,GameObject object) {
-        for (int i = 0;i < object.TextureObject.length();i++)
+        for (int i = 0;i < object.TextureObject.length() - 1;i++)
         {
             SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
             printf(" ");
@@ -32,33 +76,33 @@ public:
         printf("%s", object.TextureObject.c_str());
     }
 };
+Draw* draw;
 class Collision {
 
 };
-Draw* draw;
+
 /// <summary>
 /// Класс перемещения объекта.Тут будет все виды перемещения
 /// </summary>
 class Controller {
+public:
     /// <summary>
     /// Перемещение для персонажа.Кардинальных отличий не имеет,но более удобно
     /// </summary>
-    void PlayerMove(GameObject MoveObject,int speed,int direction) {
-        COORD EreaseCoord;
-        EreaseCoord.X = MoveObject.Pos.X;
-        EreaseCoord.Y = MoveObject.Pos.Y;
-        draw->EreseObject(EreaseCoord, MoveObject);
+    void PlayerMove(GameObject &MoveObject,int speed,int directionX,int directionY) {
 
-        COORD DrawCoord;
-        DrawCoord.X = MoveObject.Pos.X + direction + speed;
-        DrawCoord.Y = MoveObject.Pos.Y + direction + speed;
-        draw->DrawObject(DrawCoord, MoveObject);
+        draw->EreseObject(MoveObject.Pos, MoveObject);
+
+        MoveObject.Pos.X += speed * directionX;
+        MoveObject.Pos.Y += speed * directionY;
+        draw->DrawObject(MoveObject.Pos, MoveObject);
     }
     /// <summary>
     /// Перемещение для других объектов
     /// </summary>
     void ObjectMove();
 };
+Controller* controller;
 /// <summary>
 /// Стартовая постройка уровня.Срабатывает только в старте
 /// </summary>
@@ -68,6 +112,7 @@ public:
         printf("%s",GameLine.c_str());
     }
 };
+StartBuildLevel* startBuildLevel;
 /// <summary>
 /// Постройка уровня для нового кадра
 /// </summary>
@@ -75,13 +120,16 @@ class ReBuildLevel {
 public:
     void ChangeGameScene(GameObject gameObject);
 };
+ReBuildLevel* reBuildLevel;
 #pragma endregion
 #pragma region GameMethod
 void Start();
 void Tick();
 void Render();
-void GetUserInput() {
-
+char GetUserInput() {
+    char Input;
+    Input = _getch();
+    return Input ;
 }
 GameObject CreateObject(string TextureObject,int StartXPos = 0,int StartYPos = 0) {
     GameObject newObject;
@@ -106,8 +154,6 @@ list <GameObject> AllGameObjects;
 #pragma endregion
 #pragma endregion
 
-StartBuildLevel* startBuildLevel;
-ReBuildLevel* reBuildLevel;
 GameSceneInfo* gameSceneInfo;
 
 
@@ -116,6 +162,44 @@ void initializator() {
     reBuildLevel = new ReBuildLevel();
     gameSceneInfo = new GameSceneInfo();
     draw = new Draw();
+    controller = new Controller();
+}
+bool Move(GameObject &player) {
+
+    if (GetAsyncKeyState(0x41)) {
+        controller->PlayerMove(player, 1, -1, 0);
+        return true;
+    }
+    else if (GetAsyncKeyState(0x44)) {
+        controller->PlayerMove(player, 1, 1, 0);
+        return true;
+    }
+    else if (GetAsyncKeyState(0x57)) {
+        controller->PlayerMove(player, 1, 0, -1);
+        return true;
+    }
+    else if (GetAsyncKeyState(0x53)) {
+        controller->PlayerMove(player, 1, 0, 1);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+list<GameObject> AllBullet;
+bool Shoot(COORD pos) {
+    if (GetAsyncKeyState(0x20)) {
+        GameObject bullet;
+        bullet.Pos = pos;
+        bullet.TextureObject = '--';
+        AllBullet.push_back(bullet);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 int main()
@@ -132,9 +216,26 @@ int main()
 #pragma endregion
 
     //AllGameObjects.insert(CreateObject(3,3));
-    GameObject player = CreateObject("-||\n",3,3);
+    GameObject player = CreateObject("-||\n", 20, 7);
 
-
+    Timer moveTimer;
+    Timer shootTimer;
+    moveTimer.start();
+    shootTimer.start();
+    while (true)
+    {
+        while (shootTimer.elapsedMilliseconds() > 300) {
+            if (Shoot(player.Pos)) {
+                shootTimer.start();
+            }
+        }
+        while (moveTimer.elapsedMilliseconds() > 50) {
+            if (Move(player)) {
+                moveTimer.start();
+            }
+        }
+    }
+       
     COORD coord;
     coord.X = 0;
     coord.Y = 25;
